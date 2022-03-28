9565fbc475038d47eb372ba6326642352b945c60
Mathew Thompson <met@methompson.com>
2022-03-28 09:14:26 -0500

    Added Koa, types, added watchmode script and basic Koa server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-import { getMsg } from '@src/msg';
+import Koa from 'koa';
 
-console.log(getMsg());
+const app = new Koa();
+
+app.use(async (ctx) => {
+  ctx.body = 'Goodbye, World!';
+});
+
+app.listen(3000);
