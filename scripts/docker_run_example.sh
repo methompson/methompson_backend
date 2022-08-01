1330fb7e1d3a9493ad24828e030765b3888ce16e
Mathew Thompson <mathewthompson@Mathews-MacBook-Pro-16.local>
2022-08-01 08:56:39 -0500

    Updated docker run example

--- scripts/docker_run_example.sh
+++ scripts/docker_run_example.sh
@@ -4,11 +4,8 @@ docker rm methompson_blog
   cd docker
   docker run \
     -p 8000:8000 \
-    -e MONGO_DB_HOST='localhost' \
-    -e MONGO_DB_USERNAME='blog-root' \
-    -e MONGO_DB_PASSWORD='blog-password' \
+    -e MONGO_DB_URI='mongodb://blog-root:blog-password@localhost' \
     -e MONGO_DB_NAME=blog \
-    -e MONGO_USE_SRV=true \
     -e CONSOLE_LOGGING=true \
     -e DB_LOGGING=true \
     -e BLOG_TYPE=mongo_db \
