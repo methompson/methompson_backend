0a4e3bae8be9ee9021e545dfedf8919c3d357374
Mat Thompson <met@methompson.com>
2023-03-09 10:57:03 -0600

    Updated formidable version. Updated dev script to set the environment to development. Updated tsup config to use development as the environment to perform minification Updated the compile script to modify the formidable import rather than the output. This allows us to use all minification features and not just some.

--- scripts/bundle_compile.sh
+++ scripts/bundle_compile.sh
@@ -1,5 +1,7 @@
+sed -i '' "s/require('hexoid')/require('hexoid\/dist\/index.js')/g" node_modules/formidable/src/Formidable.js
+
 npm run compile_bundle
 
 # This is required because the hexoid module will import as an esmodule, despite being imported in the file as commonjs module.
 # This is caused by the package.json for hexoid defining both the commonjs and esmodule imports.
-sed -i '' "s/hexoid(25)/(typeof hexoid === 'function' ? hexoid : hexoid.default)(25)/g" dist/main.js
+# sed -i '' "s/hexoid(25)/(typeof hexoid === 'function' ? hexoid : hexoid.default)(25)/g" dist/main.js
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: ['./src/main.ts'],
-  splitting: true,
-  sourcemap: true,
-  clean: true,
-  noExternal: [/(.*)/],
-  target: ['node16'],
-  minifyWhitespace: true,
-  // minifyIdentifiers: true, // Does not work. Result throws an error
-  minifySyntax: true,
+export default defineConfig((options) => {
+  const prod = options.env?.NODE_ENV !== 'development';
+
+  return {
+    entry: ['./src/main.ts'],
+    splitting: true,
+    sourcemap: true,
+    clean: true,
+    noExternal: [/(.*)/],
+    target: ['node16'],
+    minifyWhitespace: prod,
+    minifySyntax: prod,
+    treeshake: prod,
+    minifyIdentifiers: prod,
+  };
 });
