8f553f6de6f5b7f738e9e573fb2bfb7c723444e6
Mat Thompson <met@methompson.com>
2024-04-09 09:00:32 -0500

    Added budget module to app. Began framework for budget controller testing

---
+++ src/budget/budget.module.ts
@@ -0,0 +1,32 @@
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { FactoryProvider, Module } from '@nestjs/common';
+
+import { LoggerModule } from '@/src/logger/logger.module';
+
+import { InMemoryBudgetService } from './services/budget.service.memory';
+import { BudgetController } from './controllers/budget.controller';
+
+const budgetFactory: FactoryProvider = {
+  provide: 'BUDGET_SERVICE',
+  useFactory: async (configService: ConfigService) => {
+    const type = configService.get('budgetType');
+
+    // if (type === 'file') {
+    //   const path = configService.get('viceBankFilePath');
+    //   if (typeof path === 'string') {
+    //     const service = await FileBudgetService.init(path);
+    //     return service;
+    //   }
+    // }
+
+    return new InMemoryBudgetService();
+  },
+  inject: [ConfigService],
+};
+
+@Module({
+  imports: [LoggerModule, ConfigModule],
+  controllers: [BudgetController],
+  providers: [budgetFactory],
+})
+export class BudgetModule {}
--- src/budget/controllers/budget.controller.test.ts
+++ src/budget/controllers/budget.controller.test.ts
@@ -1,1 +1,50 @@
-describe('Budget Controller', () => {});
+describe('Budget Controller', () => {
+  describe('getBudgets', () => {
+    test('gets actions from the ActionsService', async () => {});
+    test('throws an error if the userId is not a string', async () => {});
+    test('throws an error if getActions throws an error', async () => {});
+  });
+
+  describe('addBudget', () => {
+    test('adds a deposit conversion using the ActionsService', async () => {});
+
+    test('throws an error if the body is not a record', async () => {});
+
+    test('throws an error if the body cannot be parsed', async () => {});
+
+    test('throws an error if addAction throws an error', async () => {});
+  });
+
+  describe('updateBudget', () => {
+    test('updates a deposit conversion using the ActionsService', async () => {});
+    test('throws an error if the body is not a record', async () => {});
+    test('throws an error if the body cannot be parsed', async () => {});
+    test('throws an error if updateAction throws an error', async () => {});
+  });
+
+  describe('deleteBudget', () => {
+    test('deletes a deposit conversion using the ActionsService', async () => {});
+    test('throws an error if the userId is not a string', async () => {});
+    test('throws an error if deleteAction throws an error', async () => {});
+  });
+
+  describe('getCategories', () => {});
+  describe('addCategory', () => {});
+  describe('updateCategory', () => {});
+  describe('deleteCategory', () => {});
+
+  describe('getExpenses', () => {});
+  describe('addExpense', () => {});
+  describe('updateExpense', () => {});
+  describe('deleteExpense', () => {});
+
+  describe('getDeposits', () => {});
+  describe('addDeposit', () => {});
+  describe('updateDeposit', () => {});
+  describe('deleteDeposit', () => {});
+
+  describe('getWithdrawals', () => {});
+  describe('addWithdrawal', () => {});
+  describe('updateWithdrawal', () => {});
+  describe('deleteWithdrawal', () => {});
+});
