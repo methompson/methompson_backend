23a30fe0a21811847525d8180f59dd161a09b3a9
Mat Thompson <met@methompson.com>
2024-04-12 09:38:39 -0500

    Added budget file service to budget module

--- src/budget/budget.module.ts
+++ src/budget/budget.module.ts
@@ -5,19 +5,20 @@ import { LoggerModule } from '@/src/logger/logger.module';
 
 import { InMemoryBudgetService } from './services/budget.service.memory';
 import { BudgetController } from './controllers/budget.controller';
+import { FileBudgetService } from './services/budget.service.file';
 
 const budgetFactory: FactoryProvider = {
   provide: 'BUDGET_SERVICE',
   useFactory: async (configService: ConfigService) => {
     const type = configService.get('budgetType');
 
-    // if (type === 'file') {
-    //   const path = configService.get('viceBankFilePath');
-    //   if (typeof path === 'string') {
-    //     const service = await FileBudgetService.init(path);
-    //     return service;
-    //   }
-    // }
+    if (type === 'file') {
+      const path = configService.get('budgetFilePath');
+      if (typeof path === 'string') {
+        const service = await FileBudgetService.init(path);
+        return service;
+      }
+    }
 
     return new InMemoryBudgetService();
   },
